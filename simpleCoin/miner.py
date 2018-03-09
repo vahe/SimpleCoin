04c70a403f09d764ee3d784e90eaf0fcba43fa71
Vahe Khachikyan <vahe@live.ca>
2018-03-08 23:17:39 -0500

    Fixes issue with json loader input being bytes
    Decoding bytes to UTF-8 string now

--- simpleCoin/miner.py
+++ simpleCoin/miner.py
@@ -109,7 +109,7 @@ def mine(a, blockchain, node_pending_transactions):
             # Once we find a valid proof of work, we know we can mine a block so
             # ...we reward the miner by adding a transaction
             # First we load all pending transactions sent to the node server
-            NODE_PENDING_TRANSACTIONS = requests.get(MINER_NODE_URL + "/txion?update=" + MINER_ADDRESS).content
+            NODE_PENDING_TRANSACTIONS = requests.get(MINER_NODE_URL + "/txion?update=" + MINER_ADDRESS).content.decode("utf-8")
             NODE_PENDING_TRANSACTIONS = json.loads(NODE_PENDING_TRANSACTIONS)
             # Then we add the mining reward
             NODE_PENDING_TRANSACTIONS.append({
